793dd61f1d660b3674ceb69b8fa2209b4f8c666d
LuneaSomnia <raymondtexas909@gmail.com>
2025-02-09 22:13:43 +0300

    Update index.js

--- snowflake-component/pages/index.js
+++ snowflake-component/pages/index.js
@@ -2,8 +2,8 @@ import Snowflake from '../components/Snowflake';
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen"> {/* Center the snowflake */}
-      <Snowflake />
+    <div className="flex justify-center items-center h-screen bg-gray-800"> {/* Added background for contrast */}
+      <Snowflake size={300} color="white" /> {/* Adjust size and color here */}
     </div>
   );
 }
