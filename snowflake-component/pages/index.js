e4c5eaac6f8dba20157df7393cde5a3c46a391b4
LuneaSomnia <raymondtexas909@gmail.com>
2025-02-09 21:58:20 +0300

    Create index.js

---
+++ snowflake-component/pages/index.js
@@ -0,0 +1,9 @@
+import Snowflake from '../components/Snowflake';
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen"> {/* Center the snowflake */}
+      <Snowflake />
+    </div>
+  );
+}
